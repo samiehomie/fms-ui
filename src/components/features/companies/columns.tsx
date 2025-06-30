21219f7ee8868f92bb09874364e637c72ed5a647
Sam Jeong <sam@banf.ai>
2025-06-30 19:48:01 +0900

    feature: 데이터 테이블 헤더 수정

--- src/components/features/companies/columns.tsx
+++ src/components/features/companies/columns.tsx
@@ -4,7 +4,7 @@ import { ColumnDef } from '@tanstack/react-table'
 import type { Company } from '@/types/api/company.types'
 import { ShieldCheck } from 'lucide-react'
 import { formatDateTime, getCompanyTypeColor } from '@/lib/utils'
-import { MoreHorizontal, ArrowUpDown } from 'lucide-react'
+import { MoreHorizontal, ArrowUpDown, CircleSlash } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import {
   DropdownMenu,
@@ -70,36 +70,65 @@ export const columns: ColumnDef<Company>[] = [
       </Button>
     ),
     //header: () => <div className="min-w-[35px] pl-1">{'ID'}</div>,
-    cell: ({ row }) => <div className="w-full text-center pr-4">{row.getValue('id')}</div>,
+    cell: ({ row }) => (
+      <div className="w-full text-center pr-4">{row.getValue('id')}</div>
+    ),
   },
 
   { accessorKey: 'name', header: 'Company' },
   { accessorKey: 'reg_number', header: 'Reg No' },
   {
     accessorKey: 'type',
-    header: 'Type',
+    header: ({ column }) => (
+      <Button
+        variant={'ghost'}
+        className=""
+        onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+      >
+        Type
+        <ArrowUpDown className="" />
+      </Button>
+    ),
     cell: ({ row }) => {
       const type = row.getValue('type') as string
-      return <div className={`${getCompanyTypeColor(type)}`}>{type}</div>
+
+      return (
+        <Badge
+          variant="outline"
+          className={`${getCompanyTypeColor(type)} px-1.5 font-medium ml-3 `}
+        >
+          {type}
+        </Badge>
+      )
     },
   },
   { accessorKey: 'details', header: 'Details' },
   {
     accessorKey: 'verified',
-    header: () => <div className="text-center">Verified</div>,
+    header: ({ column }) => (
+      <Button
+        variant={'ghost'}
+        className="w-full"
+        onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+      >
+        Verified
+        <ArrowUpDown />
+      </Button>
+    ),
+    //header: () => <div className="text-center">Verified</div>,
     cell: ({ row }) => {
       const isVerified = row.getValue('verified')
       return (
         <div className="flex justify-center">
-          {!isVerified ? (
+          {isVerified ? (
             <Badge variant="outline" className="text-muted-foreground px-1.5">
-              <IconCircleCheckFilled className="fill-green-500 dark:fill-green-400" />
-              {`verified`}
+              <IconCircleCheckFilled className="fill-green-500" />
+              {`Verified`}
             </Badge>
           ) : (
             <Badge variant="outline" className="text-muted-foreground px-1.5">
-              <IconCircleCheckFilled className="fill-green-500 dark:fill-green-400" />
-              {`verified`}
+              <CircleSlash className="stroke-gray-500  " />
+              {`Not yet`}
             </Badge>
           )}
         </div>
