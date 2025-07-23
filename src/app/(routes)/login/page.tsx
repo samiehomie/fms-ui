a71a5b22a0fe209f1826aeef3534723a045277ed
Sam Jeong <sam@banf.ai>
2025-07-23 09:27:49 +0900

    chore: 테스트 컴포넌트 제거

--- src/app/(routes)/login/page.tsx
+++ src/app/(routes)/login/page.tsx
@@ -4,7 +4,6 @@ import { LoginForm } from '@/components/features/auth/login-form'
 export default function LoginPage() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-muted/40 p-4">
-      <div className="text-xl">TEST</div>
       <LoginForm />
     </div>
   )
