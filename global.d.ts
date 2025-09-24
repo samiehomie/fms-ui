64190595e2851826c2af41b2cc76f1c82901a00c
SAM <71742443+samiehomie@users.noreply.github.com>
2025-09-24 17:41:36 +0900

    chore: 타입 이슈 처리

--- global.d.ts
+++ global.d.ts
@@ -1,6 +1,12 @@
 import { logger as loggerUtils } from '@/lib/utils'
+
 declare global {
   var logger: typeof loggerUtils
+}
+
+declare module '*.css' {
+  const content: { [className: string]: string }
+  export default content
 }
 
 export {}
