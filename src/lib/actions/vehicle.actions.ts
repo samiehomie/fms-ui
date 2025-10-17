e18351006524e4024602ae9dbf0e41bb1c075b7a
SAM <71742443+samiehomie@users.noreply.github.com>
2025-10-17 16:35:52 +0900

    fix: 서버 액션 타입 에러 처리

--- src/lib/actions/vehicle.actions.ts
+++ src/lib/actions/vehicle.actions.ts
@@ -12,51 +12,56 @@ import { fetchServer } from '../api/fetch-server'
 
 export async function getVehicles<T = unknown>(
   params: ApiParamsType<'GET /vehicles'>,
-): Promise<ServerActionResult<T>> {
-  return await withAuthAction<T>(async (accessToken) => {
-    const { id } = params
-    const apiUrl =
-      typeof id === 'string'
-        ? buildURL(`/vehicles/${id}`)
-        : buildURL(`/vehicles`, {
-            page: params.page,
-            limit: params.limit,
-            search: params.search,
-            includeDeleted: params.include_deleted,
-          })
+) {
+  return await withAuthAction<ApiResponseType<'GET /vehicles'>>(
+    async (accessToken) => {
+      const { id } = params
+      const apiUrl =
+        typeof id === 'string'
+          ? buildURL(`/vehicles/${id}`)
+          : buildURL(`/vehicles`, {
+              page: params.page,
+              limit: params.limit,
+              search: params.search,
+              includeDeleted: params.include_deleted,
+            })
 
-    try {
-      const response = await fetch(apiUrl, {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-          'Content-Type': 'application/json',
-        },
-        cache: 'no-store',
-      })
+      try {
+        const response = await fetchServer<ApiResponseType<'GET /vehicles'>>(
+          apiUrl,
+          {
+            method: 'GET',
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+              'Content-Type': 'application/json',
+            },
+            cache: 'no-store',
+          },
+        )
 
-      if (!response.ok) {
+        if (!response.success) {
+          return {
+            success: false,
+            error: {
+              message: response.error.message || 'Unknown server error',
+              status: response.error.status,
+            },
+          }
+        }
+        const { data, pagination } = response
+
+        return { success: true, data, pagination }
+      } catch (error) {
         return {
           success: false,
           error: {
-            message: response.statusText || 'Unknown server error',
-            status: response.status,
+            message: 'Unexpected server error',
+            status: 500,
           },
         }
       }
-      const data = await response.json()
-
-      return { success: true, data }
-    } catch (error) {
-      return {
-        success: false,
-        error: {
-          message: 'Unexpected server error',
-          status: 500,
-        },
-      }
-    }
-  })
+    },
+  )
 }
 
 export async function createVehicles<T = unknown>(
