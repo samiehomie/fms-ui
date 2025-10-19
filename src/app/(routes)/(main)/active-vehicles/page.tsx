9ad8f13140c73e4aa36d7f155b182115af875be8
SAM <71742443+samiehomie@users.noreply.github.com>
2025-10-19 18:41:27 +0900

    feature: active-vehicles 페이지

---
+++ src/app/(routes)/(main)/active-vehicles/page.tsx
@@ -0,0 +1,10 @@
+//import ActiveVehiclesContent from '@/components/features/active-vehicles/active-vehicles-content'
+
+export default async function ActiveVehiclesPage() {
+  return (
+    <div className="page-container">
+      <h1 className="header-one">Active Vehicles</h1>
+      <div className="w-full">{/* <ActiveVehiclesContent /> */}</div>
+    </div>
+  )
+}
