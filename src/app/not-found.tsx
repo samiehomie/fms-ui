7ab0a512ac804c1fd997c997672e9082371a358a
SAM <71742443+samiehomie@users.noreply.github.com>
2025-11-05 14:48:31 +0900

    feature: not found 페이지 수정

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,9 +8,10 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 import { FileQuestion, Home, ArrowLeft } from "lucide-react"
-import Link from "next/link"
+import { useRouter } from "next/navigation"
 
 export default function NotFound() {
+  const router = useRouter()
   return (
     <div className="flex-1 flex items-center justify-center p-4 bg-background">
       <Card className="w-full max-w-md shadow-none ">
@@ -27,18 +28,30 @@ export default function NotFound() {
         </CardHeader>
         <CardFooter className="flex flex-col gap-2 sm:flex-row">
           <Button
-            onClick={() => window.history.back()}
+            onClick={() => {
+              window.history.back()
+            }}
             className="w-full sm:flex-1"
             variant="outline"
           >
-            <ArrowLeft className="mr-2 h-4 w-4" />
-            Go Back
+            <div className="flex items-center justify-center gap-x-2">
+              <ArrowLeft className="h-4 w-4" />
+              Go Back
+            </div>
           </Button>
-          <Button asChild className="w-full sm:flex-1" variant="default">
-            <Link href="/">
-              <Home className="mr-2 h-4 w-4" />
+          <Button
+            asChild
+            className="w-full sm:flex-1"
+            variant="default"
+            onClick={() => {
+              router.refresh()
+              router.push("/")
+            }}
+          >
+            <div className="flex items-center justify-center gap-x-2">
+              <Home className=" h-3 w-3" />
               Go Home
-            </Link>
+            </div>
           </Button>
         </CardFooter>
       </Card>
